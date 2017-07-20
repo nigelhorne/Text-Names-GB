2fad342a44730cd40b32c977a7bae2e277b9867c
Nigel Horne <njh@bandsman.co.uk>
2017-07-20 10:49:12 -0400

    Release 0.02

--- lib/Text/Names/GB.pm
+++ lib/Text/Names/GB.pm
@@ -70,11 +70,11 @@ Text::Names::GB - Perl extension for proper name parsing, normalization, recogni
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
